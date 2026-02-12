5c37548453402234368c9ca1d96535e75e60e9c9
Rozana-IM <rozanaim09@gmail.com>
2026-02-12 18:00:49 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,19 +1,22 @@
 const express = require("express");
+const cors = require("cors"); 
 const app = express();
-
-const PORT = process.env.PORT || 4000;
-
-app.get("/users", (req, res) => {
+const PORT = process.env.PORT || 5000;
+app.use(cors({
+  origin: "https://rozana-projects.online", // replace with real frontend domain
+}));
+ 
+app.get("/orders", (req, res) => {
   res.json([
-    { id: 1, name: "Siva" },
-    { id: 2, name: "DevOps User" }
+    { orderId: 101, product: "Laptop" },
+    { orderId: 102, product: "Phone" }
   ]);
 });
-
+ 
 app.get("/health", (req, res) => {
   res.send("Order Service is healthy");
 });
-
-app.listen(PORT, () =>
-  console.log(`Order Service running on port ${PORT}`)
-);
+ 
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(⁠ Order Service running on port ${PORT} ⁠);
+});
