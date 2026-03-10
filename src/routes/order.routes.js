d09f512e94713aefa8a02ffcb325349781112315
Rozana-IM <rozanaim09@gmail.com>
2026-03-10 17:03:56 +0530

    Update order.routes.js

--- src/routes/order.routes.js
+++ src/routes/order.routes.js
@@ -2,15 +2,20 @@ const express = require("express");
 const router = express.Router();
 
 const { verifyToken } = require("../middleware/auth.middleware");
+
 const {
   createOrder,
   getOrdersByUser,
   getAllOrders,
+  getOrderDetails
 } = require("../controllers/order.controller");
 
-/* USER */
 router.post("/orders", verifyToken, createOrder);
+
 router.get("/orders", verifyToken, getOrdersByUser);
+
+router.get("/orders/:id", verifyToken, getOrderDetails);
+
 router.get("/admin/orders", verifyToken, getAllOrders);
 
 module.exports = router;
