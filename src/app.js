08fac372713a7971addeed87c5b7523c676eecb7
Rozana-IM <rozanaim09@gmail.com>
2026-03-11 17:05:51 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -6,36 +6,26 @@ const orderRoutes = require("./routes/order.routes");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// ================= Middleware =================
 app.use(express.json());
 
-app.use(
-  cors({
-    origin: process.env.FRONTEND_URL || "https://rozana-projects.online",
-    methods: ["GET", "POST"],
-    credentials: true,
-  })
-);
+app.use(cors({
+  origin: process.env.FRONTEND_URL || "https://rozana-projects.online",
+  credentials: true
+}));
 
-// ================= DB =================
+// DB connection
 db.connect();
 
-// ================= Start SQS Worker =================
-require("./workers/payment.worker");
+/* HEALTH ROUTE (PUBLIC) */
 
-// ================= Routes =================
-app.use(orderRoutes);
-
-// ================= Health =================
-app.get("/health", (req, res) => {
-  res.status(200).send("Order Service is healthy");
-});
-
-app.get("/orders/health", (req, res) => {
+app.get("/orders/health", (req,res)=>{
   res.send("Order Service healthy");
 });
 
-// ================= Start =================
-app.listen(PORT, "0.0.0.0", () => {
+/* ORDER ROUTES (PROTECTED) */
+
+app.use(orderRoutes);
+
+app.listen(PORT,"0.0.0.0",()=>{
   console.log(`✅ Order Service running on port ${PORT}`);
 });
