09aa07bb3b9aae125bc07056db3d66eba3d90655
Rozana-IM <rozanaim09@gmail.com>
2026-03-30 20:49:41 +0530

    Update order.routes.js

--- src/routes/order.routes.js
+++ src/routes/order.routes.js
@@ -1,14 +1,19 @@
 const express = require("express");
 const router = express.Router();
+
 const {
   createOrder,
   getOrdersByUser,
   getOrderDetails,
+  getAllOrders
 } = require("../controllers/order.controller");
 
-// 🔥 TEMP - NO AUTH FOR TESTING
+// USER
 router.post("/", createOrder);
 router.get("/", getOrdersByUser);
 router.get("/:id", getOrderDetails);
+
+// ADMIN
+router.get("/admin/all", getAllOrders);
 
 module.exports = router;
