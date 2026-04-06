4fd56ae9624ca22f7246c926eea816d3d2e21e30
Rozana-IM <rozanaim09@gmail.com>
2026-04-07 00:33:02 +0530

    Update email.service.js

--- src/services/email.service.js
+++ src/services/email.service.js
@@ -1,7 +1,7 @@
 const { SESClient, SendEmailCommand } = require("@aws-sdk/client-ses");
 
 const client = new SESClient({
-  region: "ap-south-1",
+  region: "us-east-1",
   credentials: {
     accessKeyId: process.env.AWS_ACCESS_KEY,
     secretAccessKey: process.env.AWS_SECRET_KEY
@@ -9,28 +9,33 @@ const client = new SESClient({
 });
 
 exports.sendOrderEmail = async (to, order) => {
-
-  const params = {
-    Source: process.env.EMAIL_FROM,
-    Destination: {
-      ToAddresses: [to]
-    },
-    Message: {
-      Subject: {
-        Data: "🛍️ LUCCI Order Confirmation"
+  try {
+    const params = {
+      Source: process.env.EMAIL_FROM,
+      Destination: {
+        ToAddresses: [to]
       },
-      Body: {
-        Html: {
-          Data: `
-            <h2>Order Confirmed 🎉</h2>
-            <p><b>Order ID:</b> ${order.id}</p>
-            <p><b>Total:</b> ₹${order.total}</p>
-            <p>Status: ${order.status}</p>
-          `
+      Message: {
+        Subject: {
+          Data: "🛍️ LUCCI Order Confirmation"
+        },
+        Body: {
+          Html: {
+            Data: `
+              <h2>Order Confirmed 🎉</h2>
+              <p><b>Order ID:</b> ${order.id}</p>
+              <p><b>Total:</b> ₹${order.total}</p>
+              <p>Status: ${order.status}</p>
+            `
+          }
         }
       }
-    }
-  };
+    };
 
-  await client.send(new SendEmailCommand(params));
+    const res = await client.send(new SendEmailCommand(params));
+    console.log("✅ EMAIL SENT:", res);
+
+  } catch (err) {
+    console.error("❌ EMAIL FAILED:", err);
+  }
 };
